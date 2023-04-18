bab1af5bf04b9fdfaa743fc49138d7901f4ecbd3
Roberto Cavalcanti <63593378+rbcer17@users.noreply.github.com>
2023-04-18 13:03:09 -0300

    Add files via upload

---
+++ modulo-04/correlacao-regressao-exemplo-2023-01.R
@@ -0,0 +1,86 @@
+
+####### Aula Regressão Linear
+
+# Regressão Linear
+# Quanto que a variável y varia em relação a variavel x
+# Porém antes disso, vamos falar de:
+
+## Correlação
+# Se as variáveis estão correlacionadas, ou seja, o grau pelo qual duas 
+# Variaveis tendem a mudar juntas.
+# É dado pelo coeficiente de correlação "r"
+
+## Pearson
+# parametrico
+# preferencialmente contínuas
+
+# Spearman
+# uando não existe normalidade e/ou nao existe relação linear
+
+# Kendall
+# se existe correlação entre duas variáveis ordinais,usando uma escala ou gradiente.
+# É um método adequado quando amostras têm tamanhos reduzidos,
+
+
+# Correlacao entre massa de peixes e comprimento dos otolitos
+x <-c(6.6,6.9,7.3,7.5,8.2,8.3,9.1,9.2,9.4,10.2)
+y <-c(86,92,71,74,185,85,201,283,255,222)
+shapiro.test(x)
+shapiro.test(y)
+
+# Parece que há uma correlação positiva entre as duas variáveis e podemos testar se esta correlação e real ou 
+# nao usando a função cor.test().
+# Testamos a significância da relação
+# A correlação é maior que 0? (two.sided)
+plot(x,y)
+
+cor.test(x,y,method="pearson",alternative="two.sided")
+
+
+
+
+#### Regressão linear
+# Note, que a pergunta não é se elas estão relacionadas, e sim como.
+# EM BUSCA DA EQUAÇÃO DA RETA!!
+
+#Um exemplo de regressão simples linear: dados sobre a massa de fertilizante (gm-2) aplicada (b) e a
+#colheita de grama obtida (gm-2) para cada aplicação de fertilizante (a).
+a=c(25,50,75,100,125,150,175,200,225,250)
+b=c(84,80,90,154,148,169,206,244,212,248)
+
+
+## Fazendo a regressão, ajustando o modelo linear
+regressao=lm(b~a)
+regressao
+
+# Verificando a significância e a interação das variáveis.
+summary(regressao)
+summary(aov(regressao))
+
+# Construindo a equação da reta
+# ax+b, onde a= coeficiente angular, b= intercepto
+# Para encontra-los podemos olhar a funcao acima ou usar a função
+# coef.
+coef(regressao)
+# y=0.811x+51.93
+
+# Plotando os dados
+plot(b~a)
+
+# Colocando a reta de regressão
+abline(regressao)
+
+# Adicione a formula da reta de regressao ao grafico
+text(80,200,"y=0.811+51.93")
+
+## Diagnóstico completo dos resíduos
+par(mfrow=c(2,2))
+plot(regressao)
+
+# Valores estimados de acordo com o melhor ajuste
+fitted(regressao)
+# Valores da diferença entre os valores estimados e observados
+residuals(regressao)
+l=data.frame(a=400)
+predict(regressao,l,interval="confidence")
+
---
+++ modulo-04/para-casa-regressao-2023-01.R
@@ -0,0 +1,21 @@
+#### Exercício 1
+
+# O professor Roberto Cavalcanti foi ao campo e coletou o comprimento do corpo de 5 aves e tambem o comprimento das asas
+# destes individuos. Ele quer saber qual a relacao entre essas variaveis e quanto uma varia em funcao da outra.
+# Ajude-o fazendo o teste estatistico adequado
+
+asas: 1.6, 1.5, 1.8, 1.3, 1.2 ,1.8, 1.5, 1.6, 1.7, 1.8, 1.4, 1.3, 1.8 1.6, 1.7, 1.5, 1.5, 1.1, 2.0 ,1.7, 1.9, 1.6, 1.7, 1.6, 1.4, 1.5 1.6, 1.5,1.6, 1.7, 1.5, 1.6, 1.6, 1.3, 1.4, 1.5, 1.5, 1.4 ,1.6 ,1.5, 1.4 ,1.5, 1.8 ,1.1, 1.7, 1.4, 1.1, 1.7, 1.6, 1.4
+corpo: 3.3, 1.8, 4.2, 3.3, 1.3, 1.3, 2.5, 2.3, 4.4, 3.6, 2.3, 2.8, 4.4, 2.1, 2.3, 2.2, 4.3, 4.0, 1.0, 3.4, 2.6, 3.0, 3.1, 3.1, 2.9, 2.6, 3.0, 3.2,3.8, 2.6, 2.0, 2.0, 3.4, 4.0, 2.5, 2.0, 2.2, 4.5, 2.7, 2.1, 3.1 ,3.8,2.6, 2.8, 3.3, 4.6, 2.8, 2.9, 2.3, 2.8
+
+
+### Exercicio 2
+
+# Um jornal sensacionalista anda dizendo que o aumento da frequencia media de sorvetes vendidos em uma praia esta causando aumento no ataque
+# de tubarao na mesma praia, ambos no periodo de. Verifique tal informacao e discuta a possibilidade da relacao causal sugerida procede, uma
+# vez que o resultado for confirmado. O que pode explicar a afirmacao dada, se as variaveis forem correlatas? Discorra
+# sobre o porque da utilizacao tecnica estatistica escolhida para verificar tal informacao, dentre as quais foram vistas na ultima aula,
+# em detrimento da outra.
+
+sorvete: 20,24,24,26,27,27,38,36,39,21,20,19
+tubarões: 0,0,0,0,0,1,5,6,9,1,0,0
+
