7f1fa96c836617815cd3e781608c29855976e095
Roberto Cavalcanti <63593378+rbcer17@users.noreply.github.com>
2024-07-03 20:16:40 -0300

    Update correlacao-regressao.R

--- modulo-05/correlacao-regressao.R
+++ modulo-05/correlacao-regressao.R
@@ -1,42 +1,3 @@
-﻿
-
-
-# Two way ANOVa com interacao 
-# Para analisar a interacao da especie com o local, utilizamos o 
-# simbolo :
-resultado3 <- aov(prod2 ~ fat2+local+fat2:local, data = frutos2)
-summary(resultado3)
-
-
-# Test post hoc - Teste de Tukey ----
-# Observamos que ha variacao entre as medias, mas quais medias
-# variam entre elas? Sera que todas sao diferentes ou nao?
-
-posth <-TukeyHSD(resultado2)
-posth
-plot(posth)
-
-# Quando a barra cruzar o zero no eixo x, significa que a diferenca
-# entre esses grupos nao e significativo. 
-# Se a diferencia for positiva, a media do primeiro fator e maior 
-# que do segundo fator.
-
-# Quais sao as medias que nao tem diferencias significativas?
-
-
-
-
-
-
-
-
-
-# Teste Kruskal wallis ----
-
-# Visto se tem diferenca ou nao, devemos verificar onde esta a diferenca
-# Vamos usar um Tukey nao parametrico para o kruskal wallis
-pairwise.wilcox.test(Planilha_galhas$N_galhas,Planilha_galhas$Planta)
-
 
 
 ####### Aula Regressão Linear
@@ -65,7 +26,8 @@ pairwise.wilcox.test(Planilha_galhas$N_galhas,Planilha_galhas$Planta)
 # Correlacao entre massa de peixes e comprimento dos otolitos
 x <-c(6.6,6.9,7.3,7.5,8.2,8.3,9.1,9.2,9.4,10.2)
 y <-c(86,92,71,74,185,85,201,283,255,222)
-shapiro.test()
+shapiro.test(x)
+shapiro.test(y)
 
 # Parece que há uma correlação positiva entre as duas variáveis e podemos testar se esta correlação e real ou 
 # nao usando a função cor.test().
@@ -91,36 +53,36 @@ b=c(84,80,90,154,148,169,206,244,212,248)
 
 
 ## Fazendo a regressão, ajustando o modelo linear
-regressao=lm(b~a)
-regressao
+regr=lm(b~a)
+regr
 
 # Verificando a significância e a interação das variáveis.
-summary(regressao)
-summary(aov(regressao))
+summary(regr)
+summary(aov(regr))
 
 # Construindo a equação da reta
 # ax+b, onde a= coeficiente angular, b= intercepto
 # Para encontra-los podemos olhar a funcao acima ou usar a função
 # coef.
-coef(regressao)
+coef(regr)
 # y=0.811x+51.93
 
 # Plotando os dados
 plot(b~a)
 
 # Colocando a reta de regressão
-abline(regressao)
+abline(regr)
 
 # Adicione a formula da reta de regressao ao grafico
 text(80,200,"y=0.811+51.93")
 
 ## Diagnóstico completo dos resíduos
 par(mfrow=c(2,2))
-plot(regressao)
+plot(regr)
 
 # Valores estimados de acordo com o melhor ajuste
-fitted(regressao)
+fitted(regr)
 # Valores da diferença entre os valores estimados e observados
-residuals(regressao)
+residuals(regr)
 l=data.frame(a=400)
-predict(regressao,l,interval="confidence")
+predict(regr,l,interval="confidence")
